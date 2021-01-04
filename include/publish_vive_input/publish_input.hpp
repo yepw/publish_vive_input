5c6c71a6ef91205fd3b95f0794958b316157ad31
Luis Molina <lamolina@wisc.edu>
2021-01-04 10:43:06 -0600

    Initial commit.

---
+++ include/publish_vive_input/publish_input.hpp
@@ -0,0 +1,16 @@
+#ifndef __PUBLISH_INPUT_HPP__
+#define __PUBLISH_INPUT_HPP__
+
+namespace vive_input
+{
+    class App
+    {
+    public:
+        App() {}
+
+        int run();
+    };
+
+} // namespace vive_input
+
+#endif // __PUBLISH_INPUT_HPP__
---
+++ src/publish_input.cpp
@@ -0,0 +1,9 @@
+#include "publish_vive_input/publish_input.hpp"
+
+using App = vive_input::App;
+
+
+int main(int argc, char *argv[])
+{
+
+}
